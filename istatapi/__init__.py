c460b39642392976e40edb9faeda0360a697b7e9
Giacomo Matrone <gia.matrone@gmail.com>
2023-11-12 14:49:02 +0400

    fixed init pre-versioning

--- istatapi/__init__.py
+++ istatapi/__init__.py
@@ -4,4 +4,3 @@ from istatapi.connections.discovery import *
 from .connections import *
 from .utils import *
 from istatapi.connections.retrieval import *
-from istatapi.istat_dataset_resident_balance.istat_dataset_resident_balance import IstatDatasetResidentBalance
